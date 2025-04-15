1bd3ebab78f1c1b38f2719cda23a811c0382952b
OpiumCat <150733593+OpiumCat@users.noreply.github.com>
2025-04-15 17:11:03 +0500

    Update Template C++.cpp

--- Template C++.cpp
+++ Template C++.cpp
@@ -15,6 +15,10 @@ void solve() {
 
 int32_t main() {
     ios::sync_with_stdio(0), cin.tie(0);
+#ifdef OPIUMCAT
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     int t = 1;
     //cin >> t;
     while(t--) solve();
